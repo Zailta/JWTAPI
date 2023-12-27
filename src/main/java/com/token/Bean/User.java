de907fc23ca899b4b713104e53f92f6e78456c53
Zailta <mannanbhat60@gmail.com>
2023-12-27 20:09:02 +0530

    Adding User Bean and Mock Repository

---
+++ src/main/java/com/token/Bean/User.java
@@ -0,0 +1,44 @@
+package com.token.Bean;
+
+public class User {
+	
+	private String userId;
+	private String userName;
+	private String FirstName;
+	private String lastName;
+	private String role;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/token/Repository/UserMockRepository.java
@@ -0,0 +1,26 @@
+package com.token.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.token.Bean.User;
+
+public class UserMockRepository {
+
+	private final Map<String, User> users = new HashMap<String, User>();
+	
+	public void InMemoryUserRepository() {
+		
+		User user = new User();
+		user.setUserId("JWT-675898-IMP");
+		user.setUserName("johnDoe");
+		user.setFirstName("John");
+		user.setLastName("Doe");
+		user.setRole("ROLE_USER");
+		users.put("johnDoe", user);
+	}
+	
+	public User findUserByUserName(String userName) {
+		return users.get("johnDoe");
+	}
+}
