1f3cd4ceb34b6644a7837b9f7bdcef5928e6755c
Zailta <mannanbhat60@gmail.com>
2023-12-27 20:58:23 +0530

    JWT Security Config

--- src/main/java/com/token/JWTConfiguration/JWTResponse.java
+++ src/main/java/com/token/JWTConfiguration/JWTResponse.java
@@ -1,9 +1,31 @@
 package com.token.JWTConfiguration;
 
+import java.util.Date;
+
 public class JWTResponse {
 
-	public JWTResponse() {
-		// TODO Auto-generated constructor stub
-	}
+private String Token;
+private Date date;
+private String tracker;
+public String getToken() {
+	return Token;
+}
+public void setToken(String token) {
+	Token = token;
+}
+public Date getDate() {
+	return date;
+}
+public void setDate(Date date) {
+	this.date = date;
+}
+public String getTracker() {
+	return tracker;
+}
+public void setTracker(String tracker) {
+	this.tracker = tracker;
+}
+
+
 
 }
--- src/main/java/com/token/JWTConfiguration/JWTSecurityConfig.java
+++ src/main/java/com/token/JWTConfiguration/JWTSecurityConfig.java
@@ -1,9 +1,71 @@
 package com.token.JWTConfiguration;
 
-public class JWTSecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
-	public JWTSecurityConfig() {
-		// TODO Auto-generated constructor stub
+import com.token.UserConfig.JWTUserDetailsService;
+
+
+@Configuration
+@EnableWebSecurity
+public class JWTSecurityConfig {
+	@Autowired
+	JWTEntryPoint entryPoint;
+	@Autowired
+	@Lazy
+	JWTFilter filter;
+	
+	@Bean
+	public UserDetailsService getUserDetailsService() {
+		return new JWTUserDetailsService();
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder getBCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider getAuthenticationProvider() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(getUserDetailsService());
+		authenticationProvider.setPasswordEncoder(getBCryptPasswordEncoder());
+		return authenticationProvider;
+	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, HandlerMappingIntrospector introspector) throws Exception {
+		
+		MvcRequestMatcher.Builder builder = new MvcRequestMatcher.Builder(introspector);
+		httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(authorize->{
+			authorize.requestMatchers(builder.pattern("/api/accessToken")).permitAll().anyRequest().authenticated();
+		}).exceptionHandling(exception->{
+			exception.authenticationEntryPoint(entryPoint);
+		}).sessionManagement(session->{
+			session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		}).addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+		return httpSecurity.build();
+		
+	}
+	
+	@Bean
+	public AuthenticationManager  getAuthenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
 	}
 
 }
